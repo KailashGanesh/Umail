eb14d7a2a28fe5c5cc6f2d7aec67a71f3e910974
KailashGanesh <58307685+KailashGanesh@users.noreply.github.com>
2022-11-10 03:46:43 +0530

    not needed

--- src/js/domImports.js
+++
@@ -1,4 +0,0 @@
-export const sideBar =  document.getElementById('sidebar');
-export const emailList =  document.getElementById('email-list');
-export const emailReader = document.getElementById('email-reader')
-export const inboxBtn = document.getElementById('inboxBtn');
